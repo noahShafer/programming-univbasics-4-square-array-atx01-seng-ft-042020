7bb5aebb37e1456fe8160f86cf8466add5e3f485
distributed-memory-9073 <noahshafer0211@gmail.com>
2020-04-14 21:12:10 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   count = 0
   new_array = []
   while count != array.count 
-    new_array.append(array[0] * array[0])
+    new_array.append(array[count] * array[count])
     count += 1
   end
   return new_array
