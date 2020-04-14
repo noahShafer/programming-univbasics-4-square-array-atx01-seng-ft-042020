b13727f5c52cca839fe163a3d85c8224d169fccc
distributed-memory-9073 <noahshafer0211@gmail.com>
2020-04-14 21:12:21 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,9 @@
 def square_array(array)
-  # your code here
+  count = 0
+  new_array = []
+  while count != array.count 
+    new_array.append(array[count] * array[count])
+    count += 1
+  end
+  return new_array
 end
